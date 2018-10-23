52155977a8ac7a3a62a10099d7bfb07b0218e4ba
reichenbch <rishavchandra.v16@iiits.in>
2018-10-23 23:46:27 +0530

    hostel_app_initate

---
+++ swp/hostel/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+# Create your models here.
+#Complaint Register
+#Courier
+#Hostel Leave
+
+class HostelLeave(models.Model):
+    leave_from=models.DateField(blank=True, null=True)
+    leave_to=models.DateField(blank=True, null=True)
+    student = models.ForeignKey('accounts.Student', models.DO_NOTHING, blank=True, null=True)
+    hometown=models.CharField(max_length=200)
+    reason=models.TextField()
+    timestamp = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateField(blank=True, null=True)
+    created_by = models.CharField(max_length=45, blank=True, null=True)
+    modified_at = models.DateField(blank=True, null=True)
+    modified_by = models.CharField(max_length=45, blank=True, null=True)
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.student.user.username, self.leave_from, self.leave_to, self.reason)
+
+class ComplaintRegister(models.Model):
+    idcomplaint_register = models.IntegerField(primary_key=True)
+    complaint = models.CharField(max_length=200, blank=True, null=True)
+    student = models.ForeignKey('accounts.Student', models.DO_NOTHING, blank=True, null=True)
+    room_no = models.IntegerField(blank=True, null=True)
+    completed = models.IntegerField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateField(blank=True, null=True)
+    created_by = models.CharField(max_length=45, blank=True, null=True)
+    modified_at = models.DateField(blank=True, null=True)
+    modified_by = models.CharField(max_length=45, blank=True, null=True)
+
+    def __str__(self):
+    	return '%s %s %s %s' %(self.student.user.username,self.room_no,self.complaint,self.timestamp)
+
+class Courrier(models.Model):
+    idcourrier = models.IntegerField(primary_key=True)
+    courrier_ref_no = models.CharField(max_length=45, blank=True, null=True)
+    delivery_agent = models.CharField(max_length=45, blank=True, null=True)
+    student = models.ForeignKey('accounts.Student', models.DO_NOTHING, blank=True, null=True)
+    courrier_company = models.CharField(max_length=45, blank=True, null=True)
+    expected_arrival_time = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateField(blank=True, null=True)
+    created_by = models.CharField(max_length=45, blank=True, null=True)
+    modified_at = models.DateField(blank=True, null=True)
+    modified_by = models.CharField(max_length=45, blank=True, null=True)
+
+    def __str__(self):
+    	return '%s %s %s' (self.student.user.username,self.idcourrier,self.expected_arrival_time)
