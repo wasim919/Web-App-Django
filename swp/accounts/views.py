2af744d6bd968a497d25c71e72bacbe08473be22
wasim919 <wasimwik919@gmail.com>
2018-11-02 00:24:33 +0530

    fixed the bug

--- swp/accounts/views.py
+++ swp/accounts/views.py
@@ -5,28 +5,10 @@ from django.http import HttpResponse
 # from .models import Student
 from django.contrib.auth.decorators import login_required
 
-# def student_login(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username = username, password = password)
-#         if user is not None:
-#             login(request, user)
-#             student = Student.objects.get(user = request.user)
-#             if student.is_hostel_admin:
-#                 return redirect('hostel_admin:hostel_admin_index')
-#             if 'next' in request.POST:
-#                 return redirect(request.POST.get('next'))
-#             return redirect('/dashboard/')
-#             # return render(request, 'accounts/login_success.html')
-#         else:
-#             return render(request, 'accounts/invalid.html')
-#     else:
-#         student_login_form = StudentLoginForm()
-#         print('GET request')
-#         return render(request, 'accounts/login.html', {
-#         'student_login_form': student_login_form
-#         })
+def student_login(request):
+    return render(request, 'accounts/login.html', {
+    'student_login_form': student_login_form
+    })
 
 @login_required
 def user_logout(request):
