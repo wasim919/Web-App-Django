f1c793379af60220d82f598117a11ba7d52b144d
wasim919 <wasimwik919@gmail.com>
2018-11-06 10:16:55 +0530

    testing hostel_admin

--- swp/hostel_admin/forms.py
+++ swp/hostel_admin/forms.py
@@ -10,12 +10,8 @@ class HostelAnnouncementForm(forms.ModelForm):
 
 class HostelAnnouncements(forms.ModelForm):
     announcement_title=forms.CharField(label='announcement_title',widget=forms.TextInput(attrs={"class":"form-control"}))
-    announcement=forms.CharField(label='announcement',widget=forms.Textarea)
-    # timestamp=forms.DateField(label='timestamp', widget=AdminDateWidget())
-    # created_at=timestamp=forms.DateField(label='created_at',input_formats=['%Y-%m-%d'],widget=AdminDateWidget())
-    # created_by=forms.CharField(label='created_by',widget=forms.TextInput(attrs={"class":"form-control"}))
-    # modified_at=timestamp=forms.DateField(label='modified_at',input_formats=['%Y-%m-%d'],widget=forms.DateInput(format = '%Y-%m-%d',attrs={"class":"form-control","id":"to_date","name":"date1","placeholder":"YYYY-MM-DD","type":"text"}))
-    # modified_by=forms.CharField(label='modified_by',widget=forms.TextInput(attrs={"class":"form-control"}))
+    # announcement=forms.CharField(label='announcement',widget=forms.Textarea)
+    announcement = forms.TextInput(attrs={'size': 10, 'title': 'announcement'})
     class Meta:
         model = HostelAnnouncements
         fields = ['announcement_title', 'announcement']
