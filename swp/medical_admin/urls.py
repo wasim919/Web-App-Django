5d381e4076aaddbd79dc729b98a8709c395c6230
kaustubh-pandey <kaustubh.p16@iiits.in>
2018-11-14 22:50:10 +0530

    Added Medical Leave and Appointments

--- swp/medical_admin/urls.py
+++ swp/medical_admin/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^medical_add_announcement/$', views.medical_add_announcement, name = 'medical_add_announcement'),
     url(r'^medical_add_announcement_url/$', views.medical_add_announcement_url, name = 'medical_add_announcement_url'),
     url(r'^medical_save_edit_changes/(?P<id>\d+)/$', views.medical_save_edit_changes, name = 'medical_save_edit_changes'),
+    url(r'^medical_leave_details/(?P<id>\d+)/$', views.medical_leave_details, name = 'medical_leave_details'),
 ]
