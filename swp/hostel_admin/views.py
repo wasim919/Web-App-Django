95435257651d198f88010d07220ca71b9a69f2e6
wasim919 <wasimwik919@gmail.com>
2018-11-16 10:35:09 +0530

    added hostel_admin manual order functionality

--- swp/hostel_admin/views.py
+++ swp/hostel_admin/views.py
@@ -95,12 +95,13 @@ def save_edit_changes(request, id):
         return redirect('hostel_admin:hostel_admin_dashboard')
 
 def manual_orders(request):
-    manual_orders = ManualOrder.objects.all()
-    length = len(manual_orders)
-    return HttpResponse(render_to_string('hostel_admin/manual_order.html',context={
-    'manual_orders': manual_orders,
-    'len': length,
-    }))
+    if request.method == 'GET':
+        manual_orders = ManualOrder.objects.all()
+        length = len(manual_orders)
+        return HttpResponse(render_to_string('hostel_admin/manual_order.html',context={
+        'manual_orders': manual_orders,
+        'len': length,
+        }))
 
 def add_item(request):
     if request.method == 'POST':
