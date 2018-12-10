c3b6c31a805ef8d27d00eb69fc05ce98c8cf5e7e
kaustubh-pandey <kaustubh.p16@iiits.in>
2018-12-10 22:16:35 +0530

    Soft delete

--- swp/dashboard/models.py
+++ swp/dashboard/models.py
@@ -63,7 +63,7 @@ class ImportantContacts(models.Model):
     @property
     def isDelete(self):
         return bool(self.isDeleted())
-   
+
 
 class Messages(models.Model):
     message=models.TextField()
@@ -74,7 +74,7 @@ class Messages(models.Model):
     student=models.ForeignKey('api_integration.Student', models.DO_NOTHING, blank=True, null=True)
     isDeleted = models.BooleanField(default = 0)
     def __str__(self):
-        return str(self.name)
+        return str(self.message)
 
     @property
     def isDelete(self):
